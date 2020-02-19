cff4589676f02410406dbaf2aeb3fff5c6c8a57b
Jordan Campbell <jordan.evan.campbell@gmail.com>
2020-02-19 08:13:13 +0000

    solve dna problem #1

---
+++ dna.py
@@ -0,0 +1,18 @@
+with open("dna.txt", 'r') as fh:
+    data = fh.read()
+
+A = 0
+C = 0
+G = 0
+T = 0
+for c in data:
+    if c == 'A': 
+        A += 1
+    if c == 'G':
+        G += 1
+    if c == 'C':
+        C += 1
+    if c == 'T':
+        T += 1
+
+print(A, C, G, T)
